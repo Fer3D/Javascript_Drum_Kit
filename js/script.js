8fa365a16a97c1e181091158889ac849a214ef9b
Fernando BG <ferbg9@gmail.com>
2021-12-04 15:23:51 +0100

    Works correctly

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-window.addEventListener('keydown', function (e) {
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
+
+  function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    console.log(audio);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
-    audio.currentTime = 0; //Rewind the audio to the start when touch fast
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
     audio.play();
-    console.log(key);
-})
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
