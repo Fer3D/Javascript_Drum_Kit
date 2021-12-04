d063bde5bde82c13f116e92fa4be3c196d67cb58
Fernando BG <ferbg9@gmail.com>
2021-12-04 14:43:55 +0100

    Sounds works correctly + console testing

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 window.addEventListener('keydown', function (e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     console.log(audio);
+    if (!audio) return;
+    audio.currentTime = 0; //Rewind the audio to the start when touch fast
+    audio.play();
+    console.log(key);
 })
